e9af62b6436b139e39d48a0317b6d81725914a8c
vortex99 <elvislobo12@gmail.com>
2021-05-09 20:06:17 +0530

    chore: rename ids

--- app/src/main/java/com/shinrinyoku/expenker/HomeActivity.kt
+++ app/src/main/java/com/shinrinyoku/expenker/HomeActivity.kt
@@ -35,7 +35,7 @@ class HomeActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
+                R.id.nav_home, R.id.nav_upload_expense, R.id.nav_view_expense), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
